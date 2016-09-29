4afb22b9e70e794275be1e432560443f94d1e6c0
jeszytanada <m.tanada@klab.com>
2016-09-29 11:26:36 +0800

    Inventory prototype, first commit

---
+++ pjt_inventory/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PjtInventoryConfig(AppConfig):
+    name = 'pjt_inventory'
---
+++ pjt_inventory/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class Product(models.Model):
+    # models.ForeignKey('auth.User')
+    #id = models.ForeignKey(ForeignKey=True)
+    name = models.TextField()
+    size = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    manufacturer = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    barcode = models.CharField(max_length=100)
+    retail_price = models.BigIntegerField()
+    wholesale_price = models.BigIntegerField()
+    weight = models.CharField(max_length=100)
+    description = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
